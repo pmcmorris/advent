64fc5ca9f44e96b62cc3453c520a0cd67d8930ee
Patrick McMorris <patrick@mcmorris.ca>
2023-12-10 13:34:26 -0800

    runs on dummy data

--- 2023/day_01/src/main.rs
+++ 2023/day_01/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::fs;
+
+fn first_last_digit(s: &str) -> Option<(u64, u64)> {
+    let chars = s.chars().collect::<Vec<char>>();
+
+    for c in &chars {
+        if let Some(v) = c.to_digit(10) {
+            let first = v as u64;
+            for c in chars.iter().rev() {
+                if let Some(v) = c.to_digit(10) {
+                    let last = v as u64;
+                    return Some((first, last));
+                }
+            }
+        }
+    }
+
+    None
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let data: String = fs::read_to_string("data/example.txt")?;
+    let mut sum = 0;
+    for line in data.lines() {
+        if let Some((first, last)) = first_last_digit(line) {
+            let value = first * 10 + last;
+            println!("{line}: ({first}, {last})={value}");
+            sum += value;
+        } else {
+            panic!("error: no digits");
+        }
+    }
+
+    println!("sum: {sum}");
+    Ok(())
 }
