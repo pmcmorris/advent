7704a78f8381424387c9fe3cc608f60591a06d02
Patrick McMorris <patrick@mcmorris.ca>
2023-12-10 13:59:11 -0800

    frame up part 2

--- 2023/day_01/src/main.rs
+++ 2023/day_01/src/main.rs
@@ -18,7 +18,7 @@ fn first_last_digit(s: &str) -> Option<(u64, u64)> {
     None
 }
 
-fn part_one(data: &str) {
+fn part_one_sum(data: &str) -> u64 {
     let mut sum = 0;
     for line in data.lines() {
         if let Some((first, last)) = first_last_digit(line) {
@@ -26,14 +26,36 @@ fn part_one(data: &str) {
             //println!("{line}: ({first}, {last})={value}");
             sum += value;
         } else {
-            panic!("error: no digits");
+            panic!("error: no digits in {}", line);
         }
     }
 
-    println!("part one sum: {sum}");
+    sum
+}
+
+fn part_two_sum(_data: &str) -> u64 {
+    let sum = 0;
+
+    // todo
+
+    sum
 }
 
 fn main() {
-    let data: String = fs::read_to_string("data/input.txt").unwrap();
-    part_one(&data);
+    let example_1: String = fs::read_to_string("data/example_1.txt").unwrap();
+    let example_2: String = fs::read_to_string("data/example_2.txt").unwrap();
+    let input: String = fs::read_to_string("data/input.txt").unwrap();
+
+    println!("part one:");
+    println!("    example 1: {}", part_one_sum(&example_1));
+    //println!("    example 2: {}", part_one_sum(&example_2));
+    println!("    input:     {}", part_one_sum(&input));
+    println!("");
+
+    println!("part two:");
+    println!("    example 1: {}", part_two_sum(&example_1));
+    println!("    example 2: {}", part_two_sum(&example_2));
+    println!("    input:     {}", part_two_sum(&input));
+    println!("");
+
 }
